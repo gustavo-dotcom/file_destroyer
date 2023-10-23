84aa0dd30f559b72e86643a90b6aefffca147fbd
Gustavo <gustavo.oliveira2003@hotmail.com>
2023-10-23 17:36:01 -0300

    Add input method

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import os
 
 # file_name needs to contain one word or the full name of the file to be deleted
 # for example, if you want to delete all png files: "*.png"
-files_name = "name"
+files_name = input("type the file name or a key word for it: ")
 # the file_path doesn't need to be the exact path where the file(s) is, but be careful
 # because this script could delete files from all directories forward the path
-file_path = "/your/path/"
+file_path = input("Paste the path: ")
 files_found = []
 
 

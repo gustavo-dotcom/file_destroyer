cb1cd49876aa83a8722e8f84613fcb2c9e3c1b1c
Gustavo <gustavo.oliveira2003@hotmail.com>
2023-10-27 23:41:34 -0300

    code organization

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import fnmatch
 import os
 
-# file_name needs to contain one word or the full name of the file to be deleted
-# for example, if you want to delete all png files: "*.png"
-files_name = input("type the file name or a key word for it: ")
-# the file_path doesn't need to be the exact path where the file(s) is, but be careful
-# because this script could delete files from all directories forward the path
-file_path = input("Paste the path: ")
-files_found = []
-
 
 def find(pattern, path):
     for root, dirs, files in os.walk(path):
@@ -18,6 +10,13 @@ def find(pattern, path):
     return files_found
 
 
+# file_name needs to contain one word or the full name of the file to be deleted
+# for example, if you want to delete all png files: "*.png"
+files_name = input("File name: ")
+# the file_path doesn't need to be the exact path where the file(s) is, but be careful
+# because this script could delete files from all directories forward the path
+file_path = input("File path: ")
+files_found = []
 find(files_name, file_path)
 for file in files_found:
     os.remove(file)
